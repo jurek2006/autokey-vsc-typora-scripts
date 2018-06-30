7a601ffcc4dcea9d849f826ffd47568b57e25f08
Jurek <jurek2006@gmail.com>
2018-06-30 07:34:40 +0200

    Dodawanie znacznika (np. ...) przed i po kopiowanym fragmencie kodu

--- copyFromVSCtoTypora.py
+++ copyFromVSCtoTypora.py
@@ -1,15 +1,20 @@
 # Skrypt do kopiowania fragmentów kodu z VSC do Typory, tak, żeby mogła rozpoznać nazwę pliku przy wklejaniu
+# Skrypt do kopiowania fragmentów kodu, więc dodaje ... przed i po fragmencie kodu
+# Jeśli kopiowanie nie odbywa się z Visual Studio Code to wyświetla okno ostrzeżenia i kopiuje "normalnie"
 # Należy go umieścić w ~/.config/autokey/data
 
+programName = "Visual Studio Code"
 filenameEndMarkup = "[/title]" #znacznik końca nazwy pliku dołączanej przed treścią zaznaczenia
+snippetStart = "...\n" #znacznik dodawany przed fragmentem kopiowanego kodu 
+snippetEnd = "\n..." #znacznik dodawany po fragmencie kopiowanego kodu 
+
 winTitle = window.get_active_title() #pobranie tytułu okna w którym uruchomiono skrypt
 selection = clipboard.get_selection()
 
-if winTitle.find("Visual Studio Code") > -1:
-    toClipboard = winTitle + filenameEndMarkup + selection
+if winTitle.find(programName) > -1:
+    toClipboard = winTitle + filenameEndMarkup + snippetStart + selection + snippetEnd
     clipboard.fill_clipboard(toClipboard)
-    dialog.info_dialog("Kopiowanie", "Kopiowanie z\n'%s'" % (toClipboard))
 else:
     clipboard.fill_clipboard(selection)
-    dialog.info_dialog("Błąd", "Kopiowanie specjalne działa tylko w '%s'. Skopiowano normalnie" % ("VSC"))
+    dialog.info_dialog("Błąd", "Kopiowanie specjalne działa tylko w '%s'. Skopiowano normalnie" % (programName))
     
